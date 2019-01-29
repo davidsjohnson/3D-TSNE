1df1f92b087fefbfafa7022d42282eaa77e0aa26
David Johnson <fortjay81@gmail.com>
2019-01-28 20:07:07 -0800

    TNSE with Sound
    Implements a new OSC-XR trigger object to play audio files in ChucK

---
+++ Assets/Scripts/OSC3D/ObjectScripts/OSCTriggerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using VRTK;
+using DxR;
+
+public class OSCTriggerController : OSCTransmitterObjectBase
+{
+
+    [Header("Trigger Pointers")]
+    [Tooltip("Pointer to Trigger the Object (one of right hand or left hand must be populated)")]
+    public VRTK_DestinationMarker leftHandPointer;
+    [Tooltip("Pointer to Trigger the Object (one of right hand or left hand must be populated)")]
+    public VRTK_DestinationMarker rightHandPointer;
+
+    public static int counter = 0;
+
+    public Mark mark;
+
+    public void OnEnable()
+    {
+        controllerID = counter;
+        counter++;
+        mark = GetComponent<Mark>();
+    }
+
+    public void Start()
+    { 
+        oscAddress = string.IsNullOrEmpty(oscAddress) ? "/trigger" : oscAddress;
+    }
+}
